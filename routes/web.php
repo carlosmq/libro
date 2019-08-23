bc7f53aea6f79c010a2540163f78f4c4d38bf2f5
carlosmq <creativeria@aol.com>
2019-08-23 01:47:02 -0500

    adds author resource

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,10 @@
 |
 */
 
+use App\Providers\RouteServiceProvider;
+
 Route::post('/books','BooksController@store');
 Route::patch('/books/{book}', 'BooksController@update');
+Route::delete('/books/{book}', 'BooksController@destroy');
+
+Route::post('author', 'AuthorsController@store');
